3983b387925a60e45c9c8a8123e47a2c0b30ca65
jCold0ne <lukepierson@hotmail.com>
2019-08-30 11:54:14 +1000

    changed text colour to white to imporve readability

--- index.rb
+++ index.rb
@@ -1,8 +1,9 @@
 require "colorize"
 
-puts "This is awesome".colorize(color: :red, background: :blue)
+puts "This is awesome".colorize(color: :white, background: :blue)
 
 #saving in git - git add . = save everything | git add index.rb moves the file from 'working into staging'
 # git commmit -m "inital commit" << meaningful message of what you updated. 
 #git log = log of all the commits amde
-#git status 
+#git status = has anything been modified 
+# git diff tells you what changes have been made between versions 
