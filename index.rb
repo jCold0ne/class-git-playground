e85235fb100268d1755ca9491c6410b5acb6aa08
jCold0ne <lukepierson@hotmail.com>
2019-08-30 11:47:55 +1000

    changed background to blue becuase it made it harder to read

--- index.rb
+++ index.rb
@@ -1,3 +1,8 @@
 require "colorize"
 
-puts "This is awesome".colorize(color: :red, background: :black)
+puts "This is awesome".colorize(color: :red, background: :blue)
+
+#saving in git - git add . = save everything | git add index.rb moves the file from 'working into staging'
+# git commmit -m "inital commit" << meaningful message of what you updated. 
+#git log = log of all the commits amde
+#git status 
