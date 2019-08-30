1e32d9453582ce7a3ee51bbe4b2936a985fdd9e5
jCold0ne <lukepierson@hotmail.com>
2019-08-30 12:18:29 +1000

    changes

--- index.rb
+++ index.rb
@@ -2,8 +2,11 @@ require "colorize"
 
 puts "This is awesome".colorize(color: :white, background: :blue)
 
+#git init = create new repository | dont create a repository in a repository. 
 #saving in git - git add . = save everything | git add index.rb moves the file from 'working into staging'
 # git commmit -m "inital commit" << meaningful message of what you updated. 
 #git log = log of all the commits amde
 #git status = has anything been modified 
 # git diff tells you what changes have been made between versions 
+# git branch | shows what branch you are on 
+# 95% of the time dont go back to the past to change things 
